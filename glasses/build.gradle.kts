a1b3d51e102158773d882b80fe7998e5ca168482
Anezium <anezium@gmail.com>
2026-05-28 00:47:28 +0200

    Bump version to 0.1.1

--- glasses/build.gradle.kts
+++ glasses/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.anezium.rokidrelay.glasses"
         minSdk = 31
         targetSdk = 32
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildTypes {
